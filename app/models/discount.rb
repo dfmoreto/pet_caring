e9787d11d98f76ad9363485984d76e64be3a53fc
Daniel Moreto <dfmoreto@gmail.com>
2017-11-26 17:06:57 -0200

    Create discount type field with presence validator

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,10 +3,26 @@ class Discount < ApplicationRecord
 
   validates :title, uniqueness: true, presence: true
   validates :value, uniqueness: true, presence: true
+  validates :kind, presence: true
+
+  validate :percentage_value, if: :percentage?
+
+  enum kind: { cash: 1, percentage: 2 }
 
   def fae_display_field
     title
   end
 
 
+  private
+
+
+  def cash_valeu
+    self
+  end
+
+
+  def percentage_value
+    errors.add(:value, "can't be more than 100%") if self.value > 100
+  end
 end
