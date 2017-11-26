653073bead2869b4d6daa12e297e8780092c9f67
Daniel Moreto <dfmoreto@gmail.com>
2017-11-26 11:36:04 -0200

    Add Client association with :addresses

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,7 @@ class Client < ApplicationRecord
 
   has_many :campaign_clients, dependent: :destroy
   has_many :campaigns, through: :campaign_clients
+  has_many :addresses
 
   validates :phone, presence: true
   validates :email, uniqueness: true, presence: true,
