cbba768de12676ed0547a3b7312958e5b56452a8
Daniel Moreto <dfmoreto@gmail.com>
2017-11-26 13:54:09 -0200

    Add Schedule on top bar and Manage menu

--- app/models/concerns/fae/navigation_concern.rb
+++ app/models/concerns/fae/navigation_concern.rb
@@ -31,11 +31,13 @@ module Fae
           item('Suppliers', path: admin_suppliers_path),
           item('Services', path: admin_services_path),
           item('Products', path: admin_products_path),
+          item('Schedules', path: admin_schedules_path),
           item('Sells', path: admin_sells_path),
         ]),
         item('Marketing', subitems: [
           item('Campaigns', path: admin_campaigns_path)
         ]),
+        item('+ Schedule', path: '/admin/schedules/new'),
         item('+ Sell', path: '/admin/sells/new')
       ]
     end
