d9eacbbcf1b8e4ff5f81f24d49db4d3e1f3aba85
Daniel Moreto <dfmoreto@gmail.com>
2017-11-27 01:10:48 -0200

    Update MarketingMailer to receive EmailSent object and send to view to render its image url

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -1,6 +1,7 @@
 class MarketingMailer < ApplicationMailer
-  def campaign(client, title, body)
-    @body = body
-    mail to: client.email, subject: title
+  def campaign(client, campaign, email_sent)
+    @body = campaign.body
+    @email_sent = email_sent
+    mail to: client.email, subject: campaign.title
   end
 end
