9c21720568d7154f06c606f159a7acd3b7065a31
Daniel Moreto <dfmoreto@gmail.com>
2017-11-26 23:05:19 -0200

    Update CampaignJob to receive object campaign and client and create EmailSent

--- app/jobs/campaign_job.rb
+++ app/jobs/campaign_job.rb
@@ -1,7 +1,11 @@
 class CampaignJob < ApplicationJob
   queue_as :emails
 
-  def perform(client, title, body)
-    MarketingMailer.campaign(client, title, body).deliver_now
+  def perform(campaign, client)
+    begin
+      MarketingMailer.campaign(client, campaign.title, campaign.body).deliver_now
+      campaign.emails_sent.create!(client: client)
+    rescue => e
+    end
   end
 end
