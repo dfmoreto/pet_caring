9f47d53d5b915c3ed8a62b64cb6c4377cae00cc5
Daniel Moreto <dfmoreto@gmail.com>
2017-11-26 23:06:13 -0200

    App configured to raise erro on email sending

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module PetCaring
 
     config.active_job.queue_adapter = :sidekiq
     config.action_mailer.deliver_later_queue_name = 'emails'
+
+    config.action_mailer.raise_delivery_errors = true
   end
 end
